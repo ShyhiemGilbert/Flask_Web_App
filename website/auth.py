730f10602a243a8d2c164745a881016c07c5f5c4
ShyhiemGilbert <42538473+ShyhiemGilbert@users.noreply.github.com>
2021-06-08 22:15:16 +0100

    Adding login,logout and sign up routes

--- website/auth.py
+++ website/auth.py
@@ -4,4 +4,19 @@ from flask import Blueprint
 auth = Blueprint('auth', __name__)
 
 
+# Login route
+@auth.route('/login')
+def login():
+    return "<p>Login</p>"
 
+
+# Logout route
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+
+# sign-up route
+@auth.route('/sign-up')
+def sign_up():
+    return "<p>Sign-Up</p>"
