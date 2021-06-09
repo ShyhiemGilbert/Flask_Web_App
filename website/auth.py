4c711bea79082b7760fecd9027eef19343c33084
ShyhiemGilbert <42538473+ShyhiemGilbert@users.noreply.github.com>
2021-06-09 21:17:02 +0100

    Add function to add a new account to the DB

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,16 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, url_for
+from werkzeug.utils import redirect
+
+from . import db
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # define this file is a blueprint
 auth = Blueprint('auth', __name__)
 
 
 # Login route
-@auth.route('/login', methods=['GET','POST'])
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html", boolean="True")
 
@@ -17,25 +22,25 @@ def logout():
 
 
 # sign-up route
-@auth.route('/sign-up', methods=['GET','POST'])
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
-        lastName = request.form.get('lastName')
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-       # if length of email is less than 4, then
+        # if length of email is less than 4, then
         if len(email) < 4:
             # flash a message is email is too short
             flash('Email must be greater than 4 characters.', category='error')
 
         # if length of first name is less than 2, then
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First name must be greater than 1 characters.', category='error')
 
         # if length of last name is less than 2, then
-        elif len(lastName) < 2:
+        elif len(last_name) < 2:
             flash('Last name must be greater than 1 characters.', category='error')
 
         # if both passwords do not match, then
@@ -47,6 +52,11 @@ def sign_up():
             flash('Password must be greater than 1 characters.', category='error')
 
         else:
+            new_user = User(email=email, first_name=first_name, last_name= last_name,
+                            password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account successfully created!', category='success')
+            return redirect(url_for('views.home'))
 
     return render_template("sign_up.html")
