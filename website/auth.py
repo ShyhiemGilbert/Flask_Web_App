b81558f09e687ce485e6418066fe3cf184e76da3
ShyhiemGilbert <42538473+ShyhiemGilbert@users.noreply.github.com>
2021-06-09 21:41:19 +0100

    Fixed error by formatting index's in code

--- website/auth.py
+++ website/auth.py
@@ -25,53 +25,55 @@ def login():
         else:
             flash('Email does not exist, please try again.', category='error')
 
-            return render_template("login.html", boolean="True")
+    return render_template("login.html", boolean="True")
 
-    # Logout route
-    @auth.route('/logout')
-    def logout():
-        return "<p>Logout</p>"
 
-    # sign-up route
-    @auth.route('/sign-up', methods=['GET', 'POST'])
-    def sign_up():
-        if request.method == 'POST':
-            email = request.form.get('email')
-            first_name = request.form.get('first_name')
-            last_name = request.form.get('last_name')
-            password1 = request.form.get('password1')
-            password2 = request.form.get('password2')
-            # making sure users are not applying with the same email
-            user = User.query.filter_by(email=email).first()
-            if user:
-                flash('This email already exists.', category='error')
-            # if length of email is less than 4, then
-            if len(email) < 4:
-                # flash a message is email is too short
-                flash('Email must be greater than 4 characters.', category='error')
+# Logout route
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
 
-            # if length of first name is less than 2, then
-            elif len(first_name) < 2:
-                flash('First name must be greater than 1 characters.', category='error')
 
-            # if length of last name is less than 2, then
-            elif len(last_name) < 2:
-                flash('Last name must be greater than 1 characters.', category='error')
+# sign-up route
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        # making sure users are not applying with the same email
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('This email already exists.', category='error')
+        # if length of email is less than 4, then
+        if len(email) < 4:
+            # flash a message is email is too short
+            flash('Email must be greater than 4 characters.', category='error')
 
-            # if both passwords do not match, then
-            elif password1 != password2:
-                flash('Passwords do not match.', category='error')
+        # if length of first name is less than 2, then
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 characters.', category='error')
 
-            # if length of password is less than 7, then
-            elif len(password1) < 7:
-                flash('Password must be greater than 1 characters.', category='error')
+        # if length of last name is less than 2, then
+        elif len(last_name) < 2:
+            flash('Last name must be greater than 1 characters.', category='error')
 
-            else:
-                new_user = User(email=email, first_name=first_name, last_name=last_name,
-                                password=generate_password_hash(password1, method='sha256'))
-                db.session.add(new_user)
-                db.session.commit()
-                flash('Account successfully created!', category='success')
-                return redirect(url_for('views.home'))
+        # if both passwords do not match, then
+        elif password1 != password2:
+            flash('Passwords do not match.', category='error')
 
-        return render_template("sign_up.html")
+        # if length of password is less than 7, then
+        elif len(password1) < 7:
+            flash('Password must be greater than 1 characters.', category='error')
+
+        else:
+            new_user = User(email=email, first_name=first_name, last_name=last_name,
+                            password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account successfully created!', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template("sign_up.html")
